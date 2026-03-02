6d84d042a43d6af25efa9bbefd8a4e88e9685a34
Austin Taylor <austin.taylor@vantac.com>
2026-03-02 10:29:34 -0500

    Remove Functions bundle and wrangler.toml - use 404.html for SPA routing via git auto-deploy

--- functions/[[path]].js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const response = await context.env.ASSETS.fetch(context.request);
-  if (response.status === 404) {
-    const url = new URL(context.request.url);
-    url.pathname = '/index.html';
-    return context.env.ASSETS.fetch(url);
-  }
-  return response;
-}
