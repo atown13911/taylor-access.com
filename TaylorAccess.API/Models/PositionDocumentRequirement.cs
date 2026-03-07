6d44d46fd5515b495ab84f7cffac1aa9149982b0
Austin Taylor <austin.taylor@vantac.com>
2026-03-07 14:55:26 -0500

    Frontend branch: Angular app only (removed TaylorAccess.API)

--- TaylorAccess.API/Models/PositionDocumentRequirement.cs
+++
@@ -1,9 +0,0 @@
-namespace TaylorAccess.API.Models;
-
-public class PositionDocumentRequirement
-{
-    public int Id { get; set; }
-    public int PositionId { get; set; }
-    public int DocumentCategoryItemId { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
--- TaylorAccess.API/Services/MetricCacheService.cs
+++
@@ -1,7 +0,0 @@
-namespace TaylorAccess.API.Services;
-
-public class MetricCacheService
-{
-    public void InvalidateAll() { }
-    public void Invalidate(string key) { }
-}
